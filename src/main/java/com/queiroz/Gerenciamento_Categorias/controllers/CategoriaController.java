537ed47eedddb187c84773ed6c2ba263d0df1fb5
ferqueiroz <ferqueiroz2205@gmail.com>
2026-03-19 22:07:39 -0300

    Finalizado a parte de requisições

--- src/main/java/com/queiroz/Gerenciamento_Categorias/controllers/CategoriaController.java
+++ src/main/java/com/queiroz/Gerenciamento_Categorias/controllers/CategoriaController.java
@@ -1,12 +1,44 @@
 package com.queiroz.Gerenciamento_Categorias.controllers;
 
+import com.queiroz.Gerenciamento_Categorias.models.CategoriaModel;
 import com.queiroz.Gerenciamento_Categorias.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaController {
     @Autowired
     private CategoriaService categoriaService;
+
+    @PostMapping
+    public ResponseEntity<CategoriaModel> criar(CategoriaModel categoriaModel) {
+        return ResponseEntity.status(201).body(categoriaService.criar(categoriaModel));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoriaModel>> listar() {
+        return ResponseEntity.ok().body(categoriaService.listar());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoriaModel> buscar(@PathVariable Long id) {
+        Optional<CategoriaModel> livro = categoriaService.buscar(id);
+
+        if (livro.isPresent())
+            return ResponseEntity.ok().body(livro.get());
+        else
+            return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<CategoriaModel> deletar(@PathVariable Long id) {
+        categoriaService.excluir(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/queiroz/Gerenciamento_Categorias/services/CategoriaService.java
+++ src/main/java/com/queiroz/Gerenciamento_Categorias/services/CategoriaService.java
@@ -1,11 +1,32 @@
 package com.queiroz.Gerenciamento_Categorias.services;
 
+import com.queiroz.Gerenciamento_Categorias.models.CategoriaModel;
 import com.queiroz.Gerenciamento_Categorias.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoriaService {
     @Autowired
     private CategoriaRepository categoriaRepository;
+
+    public CategoriaModel criar(CategoriaModel categoriaModel) {
+        return categoriaRepository.save(categoriaModel);
+    }
+
+    public List<CategoriaModel> listar() {
+        return categoriaRepository.findAll();
+    }
+
+    public Optional<CategoriaModel> buscar(Long id) {
+        return categoriaRepository.findById(id);
+    }
+
+    public void excluir(Long id) {
+        categoriaRepository.deleteById(id);
+    }
+
 }
